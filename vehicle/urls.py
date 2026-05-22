e3c79e7407fe933f81d5c511f2bc4530bda878ff
Ahumuza Conley Mutungi <conleymutungi@gmail.com>
2026-05-22 11:13:12 +0300

    conley's second commit

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('vehicle_reg_form/', views.add_vehicle, name='add_vehicle'),
+    path('', views.add_vehicle, name='add_vehicle'),
+    path('vehicle_reg_form/', views.add_vehicle, name='vehicle_reg_form'),
     path('vehicle_list/', views.vehicle_list, name='vehicle_list'),
     path('vehicle_list/edit/<int:pk>/', views.edit_vehicle, name="edit_vehicle")
 ]
